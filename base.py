430678df7ebf2aeb48db48555e7b938facf42897
danbrick92 <danbrickner@hotmail.com>
2018-04-08 14:37:41 +0000

    Added alarm and coverted to Python 3

--- base.py
+++ base.py
@@ -1,7 +1,16 @@
+#WIP - need to have timer enter correct input as well as time out correctly. need to depricate signal.alarm
 # This is the base file for the robot car
+import signal
+
+# Called when alarm times out
+def interrupted(signum, frame):
+    print("0")
+
+signal.signal(signal.SIGALRM, interrupted)
+
 
 def main():
-    print "Running Remote Control Car program..."
+    print("Running Remote Control Car program...")
     keepRunning = True
     count = 1
     while keepRunning == True:
@@ -9,14 +18,26 @@ def main():
         count+= 1
         if count > 10:
             keepRunning = False
-    print "Ending Remote Control Car program..."
+    print("Ending Remote Control Car program...")
 
 def update():
     keepRunning = True
-    command = raw_input("Type instructions. Type h for list of commands. Type q to exit.")
+    timeout = .01
+    signal.setitimer(signal.ITIMER_REAL,timeout)
+    command = getUserInput()
     if command.lower() == 'q':
         keepRunning = False
     return keepRunning
+
+def getUserInput():
+    command = ""
+    try:
+        command = input("Type instructions. Type q to exit.")
+        signal.alarm(0) 
+    except:
+        print("Did not receive input. Assuming no action to be taken")
+        #signal.getitimer(self.itimer) == (0.0, 0.0)
+    return command
 
 # Calls main
 if __name__ == "__main__":
